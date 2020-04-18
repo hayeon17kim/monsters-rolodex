8b898b66f04a01eda66cec13c6ddaf5d4be8a57e
hayeon17kim <qwndhs@gmail.com>
2020-04-18 16:09:01 +0900

    Updates

---
+++ precache-manifest.b35c2bbccf2fdf67ee5a49985b36555a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887e130acf48fb5c5d208c08c878927a",
+    "url": "/monsters=rolodex/index.html"
+  },
+  {
+    "revision": "f7d5214758fa3b38ae1b",
+    "url": "/monsters=rolodex/static/css/main.16d90cca.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters=rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters=rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7d5214758fa3b38ae1b",
+    "url": "/monsters=rolodex/static/js/main.b5e95d78.chunk.js"
+  },
+  {
+    "revision": "b9a2bc39a6103f747dc2",
+    "url": "/monsters=rolodex/static/js/runtime-main.5a09a657.js"
+  }
+]);
